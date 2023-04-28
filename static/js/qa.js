f5ce98b48fccf2075520b240e2b19b07d2390ab7
YYJ <68520446+YYJ-TW@users.noreply.github.com>
2023-04-28 15:10:02 +0800

    Update question

--- static/js/qa.js
+++ static/js/qa.js
@@ -1,16 +1,33 @@
 const questions = [
     {
-        question: "東海提琴夢工場在什麼時候會去華山文創？",
-        answers: ["5/18~5/20", "4/28~4/30", "4/18~4/20"],
-        correctAnswer: "4/28~4/30",
-        score: 50
-
+        question: "製作一把小提琴需要花費多久的時間？",
+        answers: ["100小時以上", "50~70小時", "30~80小時"],
+        correctAnswer: "100小時以上",
+        score: 10
     },
     {
-        question: "東海提琴夢工場在華山文創的攤位是？",
-        answers: ["北ㄧ館51攤", "西一館24攤", "西一館57攤"],
-        correctAnswer: "西一館57攤",
-        score: 50
+        question: "與提琴夢工場學習小提琴製作，需要買工具嗎？",
+        answers: ["需要，價格約5千~1萬", "需要，價格約1千~3千", "不需要，我們會提供"],
+        correctAnswer: "不需要，我們會提供",
+        score: 20
+    },
+    {
+        question: "製作提琴有沒有什麼門檻？",
+        answers: ["沒有，不限年齡", "有，學習製作一把提琴至少花費10~20萬", "有，需要花費很多體力"],
+        correctAnswer: "沒有，不限年齡",
+        score: 20
+    },
+    {
+        question: "製作提琴第一件要做的事是什麼？",
+        answers: ["鋸面板或背板", "鋸側板", "製作模板"],
+        correctAnswer: "製作模板",
+        score: 20
+    },
+    {
+        question: "拉琴時沒有聲音是為什麼？",
+        answers: ["沒有裝上音柱", "弓毛因熱度過高導致無法讓弦發聲", "弓沒有擦上松香"],
+        correctAnswer: "弓沒有擦上松香",
+        score: 30
     }
 ];
   
